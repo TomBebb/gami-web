0cb3c0071ff3eea186aa6550f8b9541b7d6e09e6
Tom Bebbington <tombebb@protonmail.com>
2024-10-02 20:04:49 +0100

    Update icon

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <button class="btn" @click="count++">+</button>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
+    <a href="https://vitejs.dev" target="_blank"> </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="/public/icon.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />

2b624258cd22e3de2ffe53ddde959c1100765327
Tom Bebbington <tombebb@protonmail.com>
2024-10-03 17:26:52 +0100

    Display features

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,40 @@
 import DownloadButton from './components/DownloadButton.vue'
 import SponsorLinks from './components/SponsorLinks.vue'
 import Heading from './components/Heading.vue'
+
+const features: { name: string; desc: string }[] = [
+  {
+    name: 'Cross-platform',
+    desc: 'Supports Windows, Mac OS X, and Linux'
+  },
+  { name: 'Free', desc: 'No hidden costs.' },
+  { name: 'Performant', desc: "Gami's UI is fast and lightweight." },
+  { name: 'Modern', desc: 'Built with the latest .NET core, supports dark mode' },
+  { name: 'Portable', desc: 'Can run from any folder' },
+  { name: 'Achievement support', desc: 'Achievement support is built-in!' },
+  { name: 'Privacy safe', desc: 'No data is stored remotely. Data is stored on your PC.' }
+]
 </script>
 
 <template>
   <Heading />
-  <SponsorLinks />
-  <button class="btn" @click="count++">+</button>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="/public/icon.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex flex-col gap-4 p-2">
+    <div class="p-3 text-xl font-bold">Features</div>
+    <div class="grid select-none grid-cols-3 gap-4">
+      <div
+        v-for="feature in features"
+        :key="feature.name"
+        class="col-span-3 flex flex-col rounded-md border-2 border-gray-200 bg-gray-100 p-4 md:col-span-2 lg:col-span-1"
+      >
+        <div class="font-semibold">
+          {{ feature.name }}
+        </div>
+        <div class="text-gray-500">
+          {{ feature.desc }}
+        </div>
+      </div>
+    </div>
+    <SponsorLinks />
+    <DownloadButton />
   </div>
-
-  <DownloadButton />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
